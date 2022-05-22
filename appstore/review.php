f2c43edc59344d5178898270adf3c1a6025035a2
IT21167614 <92454064+Nimsaramahagedara@users.noreply.github.com>
2022-05-22 14:08:30 +0530

    small update in rating function

--- appstore/review.php
+++ appstore/review.php
@@ -4,10 +4,11 @@ include 'config.php';
 
 
 if(isset($_SESSION['email'])){
+    $rate = $_POST['phprating'];
     $email = $_SESSION['email'];
     $appid = $_POST['app_id'];
     $comment = $_POST['comment'];
-    $sql = "INSERT INTO review VALUES('$appid','$email','4.7','$comment')";
+    $sql = "INSERT INTO review VALUES('$appid','$email','$rate','$comment')";
     if(mysqli_query($conn,$sql)){
         header('Location: ' . $_SERVER['HTTP_REFERER'].'&error=Review Added Successfully !');
     }
